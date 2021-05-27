a752c87e4b76f231764ab88573c413704c9e617c
Jim Ehrismann <jehrismann@mirantis.com>
2021-05-27 19:51:12 -0400

    modifying shell-env to allow for a timeout and subsequent process cancel
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

---
+++ src/main/utils/shell-env.ts
@@ -0,0 +1,63 @@
+import execa from "execa";
+import stripAnsi from "strip-ansi";
+
+interface ShellEnvVars {
+    readonly [key: string]: string;
+}
+
+interface ShellEnvVarsInternal {
+    [key: string]: string;
+}
+
+const defaultShell = (() => {
+	const env = process.env;
+
+	if (process.platform === 'darwin') {
+		return env.SHELL || '/bin/bash';
+	}
+
+	if (process.platform === 'win32') {
+		return env.COMSPEC || 'cmd.exe';
+	}
+
+	return env.SHELL || '/bin/sh';
+})();
+
+const args = [
+	'-ilc',
+	'echo -n "_SHELL_ENV_DELIMITER_"; env; echo -n "_SHELL_ENV_DELIMITER_"; exit'
+];
+
+const env = {
+	// Disables Oh My Zsh auto-update thing that can block the process.
+	DISABLE_AUTO_UPDATE: 'true'
+};
+
+const parseEnv = (env: string) => {
+	env = env.split('_SHELL_ENV_DELIMITER_')[1];
+	const ret: ShellEnvVarsInternal = {};
+
+	for (const line of stripAnsi(env).split('\n').filter((line: string) => Boolean(line))) {
+		const [key, ...values] = line.split('=');
+		ret[key] = values.join('=');
+	}
+
+	return ret;
+};
+
+export async function shellEnv(shell?: string): Promise<ShellEnvVars> {
+	if (process.platform === 'win32') {
+		return process.env;
+	}
+
+	try {
+		const {stdout} = await execa(shell || defaultShell, args, {env});
+		return parseEnv(stdout);
+	} catch (error) {
+		if (shell) {
+			throw error;
+		} else {
+			return process.env;
+		}
+	}
+};
